1ea1726f4521d2bcda97d31932bb836fd4cc8197
jovarghese07 <jo.varghese@aleph-labs.com>
2024-11-11 15:44:44 +0530

    User signup api/ created entry in db

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
@@ -12,9 +13,26 @@ export class AuthService {
     };
   }
 
-  signup(dto: AuthDto) {
-    return {
-      msg: 'I am sign up',
-    };
+  async signup(dto: AuthDto) {
+    // Generate the Hash
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credential taken');
+        }
+      }
+      throw error;
+    }
   }
 }
