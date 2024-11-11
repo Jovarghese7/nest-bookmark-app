ad1771738e6093406df441356416f266538a8337
jovarghese07 <jo.varghese@aleph-labs.com>
2024-11-11 12:08:46 +0530

    authentication dto added, used new Body decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,20 @@
-import { Controller, Post } from "@nestjs/common";
-import { AuthService } from "./auth.service";
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
-export class AuthController{
-    constructor(private authService:AuthService){}
+export class AuthController {
+  constructor(private authService: AuthService) {}
 
-    @Post('signup')
-    sgnup(){
+  @Post('signup')
+  sgnup(@Body() dto: AuthDto) {
+    console.log(dto);
 
-      return this.authService.signup()
-    }
+    return this.authService.signup();
+  }
 
-    @Post('signin')
-    signin(){
-      return  this.authService.signin()
-    }
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
 }
